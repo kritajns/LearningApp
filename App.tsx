2cf5c6a3958a10be71a8531322d4ccb4fcdd9665
Kritajna Shrestha <kritajna@esignature.com.np>
2023-10-20 16:18:47 +0545

    package added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Route from './Src/navigation/Route';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View>
-        <Text>THis is news</Text>
-        <Route />
-      </View>
+    <SafeAreaView style={{flex: 1}}>
+      <Route />
     </SafeAreaView>
   );
 }
--- Src/assets/images/index.tsx
+++ Src/assets/images/index.tsx
@@ -1,1 +1,4 @@
 export {default as Logo} from './education.jpeg';
+export {default as WelcomeUser} from './welcome.png';
+export {default as NewUser} from './newUser.png';
+export {default as VerifiedUser} from './verified.png';
