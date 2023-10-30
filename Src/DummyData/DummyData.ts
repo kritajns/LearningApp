da5b4c47f4028245db3e54ff97216df5cedae712
Kritajna Shrestha <kritajna@esignature.com.np>
2023-10-30 18:01:11 +0545

    dummy data added

---
+++ Src/DummyData/DummyData.ts
@@ -0,0 +1,37 @@
+export const LearningData = [
+  {
+    id: 1,
+    title: 'The fashion design process',
+    completion: '5/10 Chapter',
+    timeRemain: '5h 35m left',
+    status: '',
+  },
+  {
+    id: 2,
+    title: 'Interactive Computer Graphics',
+    completion: '4/10 Chapter',
+    timeRemain: '5h 00m left',
+    status: 1,
+  },
+  {
+    id: 3,
+    title: 'Information Security Fundamentals',
+    completion: '3/10 Chapter',
+    timeRemain: '4h 35m left',
+    status: '',
+  },
+  {
+    id: 4,
+    title: 'UX Design Laws',
+    completion: '5/10 Chapter',
+    timeRemain: '5h 35m left',
+    status: 2,
+  },
+  {
+    id: 5,
+    title: 'Introduction to 2D Design',
+    completion: '6/10 Chapter',
+    timeRemain: '3h 35m left',
+    status: '',
+  },
+];
