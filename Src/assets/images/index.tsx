4c9b93db000680dfc9eddcd20e52373518386610
Kritajna Shrestha <kritajna@esignature.com.np>
2023-10-30 18:00:41 +0545

    path updated

--- Src/assets/images/index.tsx
+++ Src/assets/images/index.tsx
@@ -2,3 +2,6 @@ export {default as Logo} from './education.jpeg';
 export {default as WelcomeUser} from './welcome.png';
 export {default as NewUser} from './newUser.png';
 export {default as VerifiedUser} from './verified.png';
+export {default as sampleUser} from './user-profile.png';
+export {default as Champion} from './champion.jpeg';
+export {default as SelfLearning} from './learning2.jpeg';
--- Src/screens/index.tsx
+++ Src/screens/index.tsx
@@ -3,3 +3,6 @@ export {default as Login} from './auth/login';
 export {default as Register} from './auth/register';
 export {default as Profile} from './profile';
 export {default as ForgetPwd} from './auth/forget-password';
+export {default as Dashboard} from './dashboard';
+export {default as Learning} from './recent-learning';
+export {default as Bookmark} from './bookmark';
